57aae00efea952b02d546c0079215536b95bf89e
Xinyun Jiang <45035880+tashanguanyan@users.noreply.github.com>
2020-03-01 04:32:10 +0000

    Update 2.2.Segmentation.py

--- Software/Hand Gesture Recognition/2.Pre-Process/2.2.Segmentation.py
+++ Software/Hand Gesture Recognition/2.Pre-Process/2.2.Segmentation.py
@@ -45,22 +45,22 @@ print(rows, cols)
 
 sample = np.zeros((rows, 1), dtype='uint16')
 for r in range(rows):
-    if ((result[r, 0] > 50) or (result[r, 0] < -50)):
+    if ((result[r, 0] > 75) or (result[r, 0] < -75)):
         sample[r, 0] = 1
     else:
-        if ((result[r, 1] > 50) or (result[r, 1] < -50)):
+        if ((result[r, 1] > 75) or (result[r, 1] < -75)):
             sample[r, 0] = 1
         else:
-            if ((result[r, 2] > 200) or (result[r, 2] < 100)):
+            if ((result[r, 2] > 225) or (result[r, 2] < 75)):
                 sample[r, 0] = 1
             else:
-                if ((result[r, 3] > 00) or (result[r, 3] < -50)):
+                if ((result[r, 3] > 75) or (result[r, 3] < -75)):
                     sample[r, 0] = 1
                 else:
-                    if ((result[r, 4] > 50) or (result[r, 4] < -50)):
+                    if ((result[r, 4] > 75) or (result[r, 4] < -75)):
                         sample[r, 0] = 1
                     else:
-                        if ((result[r, 5] > 50) or (result[r, 5] < -50)):
+                        if ((result[r, 5] > 75) or (result[r, 5] < -75)):
                             sample[r, 0] = 1
                         else:
                             sample[r, 0] = 0
@@ -74,8 +74,8 @@ for r in range(rows2):
 positiontest = np.zeros((rows2, 1), dtype='uint16')
 i = 0
 for r in range(rows2-1):
-    if (position[r, 0] - position_judge[r, 0] >= 30):
-        if (position[r+1, 0] - position_judge[r+1, 0] <= 25):
+    if (position[r, 0] - position_judge[r, 0] >= 20):
+        if (position[r+1, 0] - position_judge[r+1, 0] <= 2):
             positiontest[i, 0] = position[r, 0]
             i = i+1
 
@@ -87,7 +87,7 @@ savesample = np.zeros((samplenumber, 6), dtype='uint16')
 for i in range(marknumber):
     for r in range(30):
         for c in range(6):
-            savesample[(i*31+r), c] = result[(positionmark[i, 0]-5+r), c]
+            savesample[(i*31+r), c] = result[(positionmark[i, 0]-9+r), c]
 
 x = np.real(positionmark)
 save(x,'./positionmark.xlsx')
