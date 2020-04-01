4fc44f6d644f008e74f99ff1730ae29a849cd7be
Yang DING <60398870+YangDING0201@users.noreply.github.com>
2020-04-02 00:22:08 +0100

    Add files via upload

---
+++ playlistmodel.h
@@ -0,0 +1,48 @@
+
+#ifndef PLAYLISTMODEL_H
+#define PLAYLISTMODEL_H
+
+#include <QAbstractItemModel>
+
+class QMediaPlaylist;
+
+class PlaylistModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+    enum Column
+    {
+        Title = 0,
+        ColumnCount
+    };
+
+    PlaylistModel(QObject *parent = 0);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex &child) const;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+    QMediaPlaylist *playlist() const;
+    void setPlaylist(QMediaPlaylist *playlist);
+
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::DisplayRole);
+
+private slots:
+    void beginInsertItems(int start, int end);
+    void endInsertItems();
+    void beginRemoveItems(int start, int end);
+    void endRemoveItems();
+    void changeItems(int start, int end);
+
+private:
+    QMediaPlaylist *m_playlist;
+    QMap<QModelIndex, QVariant> m_data;
+};
+
+#endif // PLAYLISTMODEL_H
+
