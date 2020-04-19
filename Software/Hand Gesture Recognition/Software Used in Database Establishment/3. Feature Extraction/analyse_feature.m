6fc69bd8ce26f8ca342cc954ab4cde2ba5aeb754
Xinyun Jiang <45035880+tashanguanyan@users.noreply.github.com>
2020-04-19 02:34:27 +0100

    Update analyse_feature.m

--- Software/Hand Gesture Recognition/Software Used in Database Establishment/3. Feature Extraction/analyse_feature.m
+++ Software/Hand Gesture Recognition/Software Used in Database Establishment/3. Feature Extraction/analyse_feature.m
@@ -1,3 +1,4 @@
+# Read features of all samples collected.
 forward = table2array(feature2forward);
 backward = table2array(feature2backward);
 left = table2array(feature2left);
@@ -5,9 +6,9 @@ right = table2array(feature2right);
 up = table2array(feature2up);
 down = table2array(feature2down);
 n=1:179;
-m=1:180;
+m=1:180; # Here are a total of 180 samples for each gesture. 
 
-i = 1;
+i = 1; # The number of tested feature.
 AX1 = forward(m,i)';
 AX2 = backward(m,i)';
 AX3 = left(m,i)';
